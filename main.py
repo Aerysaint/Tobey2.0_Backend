7be3526987fab2963516061cfaefb3e364f52cea
Aviansh1234 <74455180+Aviansh1234@users.noreply.github.com>
2025-01-28 14:40:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from datetime import datetime
+
+from fastapi import FastAPI, Cookie, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.models import Response
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse, JSONResponse
+
 import firebase_handler as fh
 import geminiFunctions as gemini
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["set-cookie"]  # Explicitly expose the Set-Cookie header
+)
 
 
 @app.get("/")
@@ -32,9 +48,11 @@ async def updateNextInitial(sessionid: str):
         return -1
     response = gemini.next_message_for_initial_chat(history)
     if ("Received hihihiha" in response):
-        response = response.remove("Received hihihiha")
+        history.append({"role": "model", "parts": [{"text": response}]})
+        history.append({"role": "user", "parts": [{"text": "Please give me a summary of my trip again"}]})
+        response = gemini.next_message_for_initial_chat(history)
         fh.add_summary(sessionid, response)
-        return "information gathered"
+        return {'status': 'chat finished'}
     fh.add_message_to_first_chat("model", sessionid, response)
     return "ok"
 
@@ -77,15 +95,79 @@ async def getAllActivities(sessionid: str):
 
 
 @app.get("/createUser")
-async def createUser(username: str, email: str, userid: str):
+async def createUser(request: Request):
+    idToken = request.cookies.get("session")
+    print("token is", idToken)
+    email = fh.get_email(idToken)
+    userid = fh.get_uid(idToken)
+    username = fh.get_name_by_session_cookie(idToken)
     fh.create_user(username, email, userid)
     return "ok"
 
 
-@app.get("/createSession")
-async def createSession(userid: str):
+@app.get("/createGroup")
+async def createGroup(request: Request):
+    idToken = request.cookies.get("session")
+    userid = fh.get_uid(idToken)
     try:
-        fh.create_session(userid)
+        return {"groupId": fh.create_session(userid)}
     except:
-        return -1
-    return "ok"
+        return {"groupId": None}
+
+
+class AuthToken(BaseModel):
+    idToken: str
+
+
+@app.post("/authenticateUser")
+async def authenticateUser(token: AuthToken):
+    print("id token is", token.idToken)
+    cookie = fh.verify_id_token(token.idToken)
+    response = JSONResponse(content={"status": "ok"}, status_code=200)
+    response.set_cookie(
+        key="session",
+        value=cookie,
+        samesite="lax",
+        secure=False,
+        httponly=True,
+        path="/"
+    )
+    return response
+
+
+@app.get("/authenticateSession")
+async def authenticateSession(request: Request):
+    cookie = request.cookies.get("session")
+    print("cookie is", cookie)
+    return {"session": fh.verify_session_cookie(cookie)}
+
+@app.get("/getGroups")
+async def getGroups(request: Request):
+    cookie = request.cookies.get("session")
+    uid = fh.get_uid(cookie)
+    return fh.get_groups(uid)
+
+@app.get("/getGroupName")
+async def getGroupName(groupId: str):
+    return {"name": fh.get_group_name(groupId)}
+
+@app.get("/getGroupMemberCount")
+async def getGroupMemberCount(groupId: str):
+    return {"count": fh.get_group_member_count(groupId)}
+
+@app.get("/leaveGroup")
+async def leaveGroup(groupId: str, request: Request):
+    cookie = request.cookies.get("session")
+    userid = fh.get_uid(cookie)
+    fh.group_leave(groupId, userid)
+    return {"status": "ok"}
+
+@app.get("/joinGroup")
+async def joinGroup(groupId: str, request: Request):
+    if fh.check_group_existance(groupId) is False:
+        return {"status": "Group does not exist"}
+    cookie = request.cookies.get("session")
+    userid = fh.get_uid(cookie)
+    fh.group_join(groupId, userid)
+
+    return {"status": "ok"}
